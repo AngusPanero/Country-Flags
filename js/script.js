34e3b30731f5b6d905f3f7ff9431134e8f6d0b31
Agustin Panero <agustinpanero@MacBook-Pro-de-Agustin.local>
2024-11-28 00:17:20 +0100

    Banderas Alfabeticamente Traidas

--- js/script.js
+++ js/script.js
@@ -1,28 +1,48 @@
-divCountries = document.getElementById("countries-list")
+divCountries = document.getElementById("countries-list");
+
+let objetosFiltrados = [];
 
 const traerBanderas = async () => {
     try {
         const response = await fetch ("https://restcountries.com/v3/all");
         if (!response.ok){
             console.error("Se ha producido un Error", response.status);
-            return
+            return;
         }
-        const objetosFiltrados = [];
+        objetosFiltrados = [];
         const data = await response.json();
 
         for (let i = 0; i < data.length; i++){
-        const objFilter = {
-            bandera: data[i].flags,
-            nombre: data[i].name.common,
-            capital: data[i].capital,
-            poblacion: data[i].population,
-            carretera: data[i].car.side,
+            const objFilter = {
+                bandera: data[i].flags[0],
+                nombre: data[i].name.common,
+                capital: data[i].capital,
+                poblacion: data[i].population,
+                carretera: data[i].car.side,
             };
-            objetosFiltrados.push(objFilter)
+            objetosFiltrados.push(objFilter);
         }
+        objetosFiltrados.sort((a, b) => a.nombre.localeCompare(b.nombre));
         console.log(objetosFiltrados);
+        template();
     } catch (error){
-        console.error("Error en la Solicitud", error)
-    };
+        console.error("Error en la Solicitud", error);
+    }
 };
-traerBanderas()
+
+const template = () => {
+    divCountries.innerHTML = "";
+    objetosFiltrados.forEach(obj =>{
+        divCountries.innerHTML += `
+        <div>
+            <h3>${obj.nombre}</h3>
+            <img src="${obj.bandera}" alt="Bandera de ${obj.nombre}">
+            <p>Capital: ${obj.capital}</p>
+            <p>Población: ${obj.poblacion}</p>
+            <p>Conducen por la: ${obj.carretera}</p>
+        </div>
+        `;
+    });
+};
+
+traerBanderas();
